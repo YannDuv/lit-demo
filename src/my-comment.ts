0840621b0d19a1abf60a00426e4c6d42cef34faf
Yann Duval <yann.duval@idean.com>
2021-05-26 22:27:43 +0200

    :sparkles: my-comment

--- src/my-comment.ts
+++ src/my-comment.ts
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from "lit"
-import { customElement } from "lit/decorators.js"
+import { customElement, property, query } from "lit/decorators.js"
+import { ifDefined } from "lit/directives/if-defined"
 
 declare global {
   interface HTMLMyCommentMap {
@@ -11,7 +12,35 @@ declare global {
 export class MyComment extends LitElement {
   static styles = css``
 
+  @property({ type: String })
+  name?: string
+
+  @query("textarea")
+  textArea?: HTMLTextAreaElement
+
+  get value() {
+    return this.textArea?.value ?? ""
+  }
+
+  handleChange() {
+    this.dispatchEvent(
+      new CustomEvent<{ value: string }>("onchange", {
+        detail: { value: this.value },
+      })
+    )
+  }
+
   render() {
-    return html`<div>TODO: comment</div>`
+    return html`<div>
+      <label>
+        ${this.name}:
+        <textarea
+          name=${ifDefined(this.name)}
+          @input=${this.handleChange}
+          required
+        ></textarea>
+      </label>
+      <b>${this.value}</b>
+    </div>`
   }
 }
