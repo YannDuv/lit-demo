e38b57de91eacd1657e38290103cf1d179410b4c
Yann Duval <yann.duval@idean.com>
2021-05-26 23:01:02 +0200

    :sparkles: my-rate

--- src/my-rate.ts
+++ src/my-rate.ts
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from "lit"
-import { customElement } from "lit/decorators.js"
+import { customElement, property } from "lit/decorators.js"
+import { classMap } from "lit/directives/class-map"
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -9,9 +10,57 @@ declare global {
 
 @customElement("my-rate")
 export class MyRate extends LitElement {
-  static styles = css``
+  @property({ type: Number })
+  value?: number
+
+  @property({ type: String })
+  label?: string
+
+  @property({ type: Number })
+  max!: number
+
+  static styles = css`
+    div {
+      display: flex;
+    }
+    svg {
+      display: block;
+      margin-right: 1rem;
+    }
+    .active circle {
+      fill: orange;
+    }
+  `
+
+  handleClick(value: number) {
+    this.dispatchEvent(
+      new CustomEvent("onchange", {
+        detail: { value },
+      })
+    )
+    this.value = value
+  }
 
   render() {
-    return html`<div>TODO: rate</div>`
+    return html`<label
+      >${this.label}
+      <div>
+        ${Array(this.max)
+          .fill("x")
+          .map((_, index) => {
+            return html`<svg
+              class=${classMap({
+                active: Boolean(this.value && index <= this.value),
+              })}
+              @click=${() => this.handleClick(index)}
+              viewport="0 0 40 40"
+              height="40"
+              width="40"
+            >
+              <circle cx="20" cy="20" r="20" fill="gold" />
+            </svg>`
+          })}
+      </div>
+    </label>`
   }
 }
